5dc5efb4c44888958a143ae46d186e5da6c87584
Dylan <dcleaves@student.bridgew.edu>
2020-01-23 11:47:08 -0500

    Added the welcome message.

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+        public static void main(String[] args){
+            System.out.println("Hello Comp152");
+            System.out.println("Lets make it work");
+        }
+}
